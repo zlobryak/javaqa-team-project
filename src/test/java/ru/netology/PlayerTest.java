c8be6b0b7177488d4ed816c371b30e91b14df703
Zlobryak <clychnik@gmail.com>
2023-03-24 23:57:36 +0300

    timePlayed added close #1

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,7 +1,10 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ru.netology.exeptions.NoGameExeption;
 
 public class PlayerTest {
 
@@ -19,5 +22,27 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void timePlayed() { //Проверяю исключение
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Game 2", "TestGenre2");
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+        Assertions.assertThrows(NoGameExeption.class, () -> {
+            player.timePlayed(game2);
+        });
+    }
+
+    @Test
+    void timePlayedPositive() { // Позитивная проверка метода
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 3);
+        assertEquals(3, player.timePlayed(game));
+    }
     // другие ваши тесты
 }
