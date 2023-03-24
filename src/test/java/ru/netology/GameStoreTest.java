9366d63a8a1e99219e4e47227c22b7453c74ab37
Zlobryak <clychnik@gmail.com>
2023-03-24 23:53:37 +0300

    GameStoreTest test for issues 2, 3, 4.

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,38 +1,40 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
+    GameStore store = new GameStore();
 
     @Test
     public void shouldAddGame() {
-
-        GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         assertTrue(store.containsGame(game));
     }
 
     @Test
-    void publishGame() {
-    }
-
-    @Test
-    void containsGame() {
-    }
-
-    @Test
     void addPlayTime() {
+        store.addPlayTime("Player1", 100);
     }
 
     @Test
     void getMostPlayer() {
+        store.addPlayTime("Player1", 100);
+        store.addPlayTime("Player2", 200);
+        store.addPlayTime("Player3", 300);
+
+        Assertions.assertEquals("Player3", store.getMostPlayer());
     }
 
     @Test
     void getSumPlayedTime() {
+        store.addPlayTime("Player1", 100);
+        store.addPlayTime("Player2", 200);
+        store.addPlayTime("Player3", 300);
+        Assertions.assertEquals(300+200+100, store.getSumPlayedTime());
     }
 
-    // другие ваши тесты
 }
